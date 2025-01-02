5ec70fb5d5bae14acc7874fe75b3508f284d30b9
Mayank <166874794+Mayank7371@users.noreply.github.com>
2025-01-02 23:19:08 +0530

    add methods for post put patch and delete

--- index.js
+++ index.js
@@ -1,12 +1,25 @@
 const express = require ("express");
 const app = express();
 const http = require("http");
-
-
-app.get('/' , (req,res)=>{
-  res.send("hi there!")
+const port = 3000;
+app.get('/get' , (req,res)=>{
+  res.send("server says hello!")
+});
+app.post('/post', (req, res) => {
+  res.send({ message: 'POST request received', data: req.body });
+});
+app.put('/put', (req, res) => {
+  res.send({ message: `PUT request for ID ${req.params.id}`, data: req.body });
 });
 
-app.listen(8000,()=>{
-  console.log("server is running on port no : 8000")
+app.patch('/patch', (req, res) => {
+  res.send({ message: `PATCH request for ID ${req.params.id}`, data: req.body });
+});
+
+app.delete('/delete', (req, res) => {
+  res.send({ message: `DELETE request for ID ${req.params.id}` });
+});
+  
+app.listen(port,()=>{
+  console.log(`server is running on port : ${port}`)
 });
